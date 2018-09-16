2d7df844b576bef7691adbe4e07e18838957ad1f
Rikku <rocket.rikku@gmail.com>
2018-09-16 19:32:39 +0300

    Timer clean code

--- timer.js
+++ timer.js
@@ -1,30 +1,32 @@
 let timer;
 let timerCanceling = false;
+let intervalID
 
 function startTimer(duration, display) {
     timer = duration;
     let minutes, seconds;
-
     return setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
-
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
         display.textContent = `${minutes} : ${seconds}`;
-
-        // console.log(timer, seconds)
-        theEnd()
-
+        zeroTimer()
         if (--timer < 0) {
             timer = duration;
         }
-
     }, 1000);
-
 }
-let intervalID
+function zeroTimer() {
+    console.log("theEnd")
+    if (timer == 0) {
+        clearInterval(intervalID)
+        let display = document.querySelector('#time');
+        let text = document.getElementById("text");
+        text.innerText = "good job!";
+        display.textContent = "00 : 00";
+    }
+}
 
 window.onload = function () {
     console.log("onload")
@@ -35,38 +37,19 @@ window.onload = function () {
 
 window.onmousemove = function () {
     clearInterval(intervalID);
-
     let text = document.getElementById("text");
     text.innerText = "one more time!";
-
     if (!timerCanceling) {
         timerCanceling = true;
         setTimeout(function () {
             let threeMinutes = 60 * 3 - 1;
             let display = document.querySelector('#time');
-
             text.innerText = "keep calm for";
             display.textContent = "03 : 00"
-
             intervalID = startTimer(threeMinutes, display)
             timerCanceling = false;
         }, 3000);
     }
-
-
-
 }
-
 
 window.onkeypress = window.onmousemove
-
-function theEnd() {
-    console.log("theEnd")
-    if (timer == 0) {
-        clearInterval(intervalID)
-        let display = document.querySelector('#time');
-        let text = document.getElementById("text");
-        text.innerText = "good job!";
-        display.textContent = "00 : 00";
-    }
-}
